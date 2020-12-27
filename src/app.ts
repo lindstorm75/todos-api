d81a5b1cb17e11a0453e16c53b5a26ab9f3e568c
Thanapong Angkha <lindstorm76@outlook.com>
2020-12-27 20:25:45 +0700

    Not found returned data should be null

--- src/app.ts
+++ src/app.ts
@@ -27,7 +27,7 @@ app.get("/todos/:id", async (req: Request, res: Response): Promise<any> => {
   connectDB()
   const id: number = +req.params.id
   const data: any | null = await TodoModel.findOne({ id })
-  if (data === undefined) return res.status(400).json({})
+  if (data === null) return res.status(400).json({})
   const newObj: any = {}
   newObj.id = data.id
   newObj.username = data.username
