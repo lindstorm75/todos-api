a5b3e79beee43402814ff4d0caa44cdf01ff48c7
Thanapong Angkha <lindstorm76@outlook.com>
2020-12-28 21:20:34 +0700

    Corrected a typo

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json())
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  res.headers("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
   next()
 })
 
