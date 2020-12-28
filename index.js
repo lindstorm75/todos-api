dba4681824e1024a90450f7ec20fc2ee4946e724
Thanapong Angkha <lindstorm76@outlook.com>
2020-12-28 21:19:16 +0700

    Allowed cors delete method

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(express.json())
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  res.headers("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
   next()
 })
 
@@ -44,6 +45,7 @@ app.get("/todos/:id", async (req, res)=> {
 })
 
 app.post("/todos/", async (req, res) => {
+  console.log(req.body)
   const { username, title } = req.body
   if (!username || !title)
     res.status(400).json({ message: "Bad request." })
